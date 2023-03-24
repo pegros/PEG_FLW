fb9300b994e6a403b70968eab1f8a10d29711474
P-E Gros <pgros@salesforce.com>
2023-03-24 12:26:53 +0100

    New simple ForceRedirect screen flow component to redirect user to a record.

---
+++ force-app/main/default/lwc/sfpegForceRedirectFlw/sfpegForceRedirectFlw.js
@@ -0,0 +1,54 @@
+import { LightningElement, api } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+
+export default class SfpegForceRedirectCmp extends NavigationMixin(LightningElement) {
+
+    //----------------------------------------------------------------
+    // Main configuration fields (for Flow Builder)
+    //----------------------------------------------------------------
+    @api recordId;          // Target record Id
+    @api objectApiName;     // Target object Name (required in LWR context)
+    @api isDebug;           // Debug actiaton flag
+
+    //----------------------------------------------------------------
+    // Component Initialisation
+    //----------------------------------------------------------------
+    connectedCallback() {
+        if (this.isDebug) console.log('connected: START');
+
+        let targetPage = {
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: this.recordId,
+                actionName: 'view'
+            }
+        };
+        if (this.objectApiName) {
+            targetPage.attributes.objectApiName = this.objectApiName;
+        }
+        if (this.isDebug) console.log('connected: redirecting to targetPage ', JSON.stringify(targetPage));
+
+        if (!this.isDebug) this[NavigationMixin.Navigate](targetPage);
+
+        if (this.isDebug) console.log('connected: END');
+    }
+
+    handleClick(event) {
+        if (this.isDebug) console.log('handleClick: START');
+
+        let targetPage = {
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: this.recordId,
+                actionName: 'view'
+            }
+        };
+        if (this.objectApiName) {
+            targetPage.attributes.objectApiName = this.objectApiName;
+        }
+        if (this.isDebug) console.log('handleClick: redirecting to targetPage ', JSON.stringify(targetPage));
+
+        this[NavigationMixin.Navigate](targetPage);
+        if (this.isDebug) console.log('handleClick: END');
+    }
+}
