0a73e05730575c9015c65977b4a0e30d9bd0149e
P-E Gros <pgros@salesforce.com>
2023-04-15 19:37:04 +0200

    minor comments update

--- force-app/main/default/lwc/sfpegPageRedirectFlw/sfpegPageRedirectFlw.js
+++ force-app/main/default/lwc/sfpegPageRedirectFlw/sfpegPageRedirectFlw.js
@@ -1,3 +1,38 @@
+/* eslint-disable no-console */
+
+/***
+* @author P-E GROS
+* @date   Apr. 2023
+* @description  LWC Componentfor Flows enabling to trigger a lightning Data Service
+*               cache refresh of a set of records (modified by the flow) and redirect
+*               the user to a given page reference. Should be positioned in a last Flow
+*               page, the logic being triggered automatically upon component initiation
+*
+* Legal Notice
+* 
+* MIT License
+* 
+* Copyright (c) 2023 pegros
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+***/
+
 import { LightningElement, api } from 'lwc';
 import { NavigationMixin } from 'lightning/navigation';
 import { notifyRecordUpdateAvailable } from 'lightning/uiRecordApi';
